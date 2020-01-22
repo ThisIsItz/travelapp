3252ef29c341fde6d0cd8266007d4d7f0b5d4e6e
ThisIsItz <ItziarUnderTheSea@gmail.com>
2020-01-22 21:36:27 +0000

    first commit

---
+++ src/client/index.js
@@ -0,0 +1,8 @@
+import { checkForName } from './js/nameChecker'
+import { handleSubmit } from './js/formHandler'
+
+
+console.log(checkForName);
+
+alert("I EXIST")
+console.log("CHANGE!!");
---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    entry: './src/client/index.js',
+    module: {
+        rules: [
+                {
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+                }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
+}
