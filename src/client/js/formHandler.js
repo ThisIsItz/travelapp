b0fd69dbda408d7c018755901a87181c37910e2a
Itziar <itziarurbieta94@gmail.com>
2020-01-23 11:04:22 +0000

    Adding sass

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -8,7 +8,7 @@ function handleSubmit(event) {
     checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('http://localhost:8080/test')
     .then(res => res.json())
     .then(function(res) {
         document.getElementById('results').innerHTML = res.message
--- webpack.dev.js
+++ webpack.dev.js
@@ -17,12 +17,19 @@ module.exports = {
     },
     module: {
         rules: [
-                {
+          {
             test: '/\.js$/',
             exclude: /node_modules/,
             loader: "babel-loader"
-                }
-        ]
+          },
+          {
+            test: /\.scss$/,
+            use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+          }
+        ],
+
+
+
     },
     plugins: [
         new HtmlWebPackPlugin({
