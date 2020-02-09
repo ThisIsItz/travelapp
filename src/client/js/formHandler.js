f7e7df43be8a44f8717fde206926ba5c17f55257
ThisIsItz <itziarurbieta94@gmail.com>
2020-02-09 18:36:32 +0000

    Updating UI

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,28 +7,6 @@ function handleSubmit(event) {
     const locationInput = document.getElementById('name').value;
     const date = document.getElementById('date').value
     checkForLocation();
-    // 
-
-     // Function to post data to our server
-    const postData = async (url = '/add', data = {}) => {
-        let response = await fetch(url, {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type' : 'application/json',
-            },
-            body: JSON.stringify(data)
-        });
-
-        let newData = await response.json();
-        try {
-            console.log(newData)
-            return newData
-        } catch(error) {
-            console.log('error',error);
-        }
-    }
-    
     start();
     updateUI();
     document.getElementById('location').innerHTML = 'Mi trip to ' + locationInput.charAt(0).toUpperCase() + locationInput.slice(1);;
@@ -52,7 +30,8 @@ const getData = async () => {
 const updateUI = async () => {
     const data = await getData();
     console.log(data)
-    document.getElementById('results').innerHTML = 'My results:' + data.coordenate;
+    document.getElementById('results').innerHTML = 'The weather will be '+ data.temperature + "&degC <br>"
+    document.getElementById('photo').src = data.photo;
 
 }
 
