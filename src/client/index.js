734c37303f1a23597ccd9f89336de5a6cafc4585
ThisIsItz <itziarurbieta94@gmail.com>
2020-02-10 17:45:48 +0000

    Adding hover efect on submit button

--- src/client/index.js
+++ src/client/index.js
@@ -3,8 +3,7 @@ import { handleSubmit } from './js/formHandler'
 import { start, stopTimer } from './js/countdown'
 import './styles/resets.scss'
 import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
+import './styles/button.scss'
 import './styles/header.scss'
 
 export {
