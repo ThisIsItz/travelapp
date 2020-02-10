192308c74b91ac002f9ef593e8871681f6d2faf0
ThisIsItz <itziarurbieta94@gmail.com>
2020-02-10 18:55:37 +0000

    Fixed functions

--- src/server/index.js
+++ src/server/index.js
@@ -29,16 +29,6 @@ app.listen(4000, function () {
     console.log('Example app listening on port 4000!')
 })
 
-app.post('/add', function (req, res) {
-    console.log(req.body)
-    projectData['polarity'] = res.polarity;
-    projectData['subjectivity'] = res.subjectivity;
-    projectData['polarity_confidence'] = res.polarity_confidence;
-    projectData['subjectivity_confidence'] = res.subjectivity_confidence;
-    console.log(projectData)
-    res.send(projectData);
-});
-
 app.get('/trip/:location', (req, res) => {
     const location = req.params.location
     console.log(location)
